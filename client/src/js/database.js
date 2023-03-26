d932b597b4dc00012e1ed9d927d97c7b3934cb0c
Awele1111 <awelelan@Aweles-MacBook-Air.local>
2023-03-25 21:00:27 -0500

    fixed errors in code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -42,7 +42,7 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'read');
+  const tx = jateDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
